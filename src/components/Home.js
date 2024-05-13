d4c3525684eeafa52df1a034439d54a21fed5d94
samwanyua <samexample8@gmail.com>
2024-05-13 13:33:43 +0300

    Feat: Modify responsiveness of the components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { Link } from "react-scroll";
 
-const Home = ({authorized}) => {
-
-  console.log(authorized)
+const Home = ({ authorized }) => {
   return (
-    <div className="min-h-[70vh] flex flex-col md:flex-row md:justify-between items-center md:mx-32 mx-5 mt-10">
-      <div className="md:w-2/4 text-left mr-8 ">
-        <h2 className="text-3xl font-bold leading-tight">
-        Empowering Sales Teams with  
-          <span className="text-black font-normal">  Efficient Route Planning</span>
+    <div className="min-h-[70vh] flex flex-col lg:flex-row lg:justify-between items-center lg:mx-32 mx-5 mt-10">
+      <div className="lg:w-2/4 text-center lg:text-left mb-8 lg:mb-0">
+        <h2 className="text-3xl lg:text-4xl font-bold leading-tight">
+          Empowering Sales Teams with{" "}
+          <span className="text-black font-normal">Efficient Route Planning</span>
         </h2>
-        <p className="text-lightText mt-5 text-start">
-        Welcome to our platform dedicated to optimizing route planning for sales professionals. 
-        Join us on this journey as we revolutionize the way salespeople navigate through their daily tasks 
-        and gather crucial product insights. Together, let's make route planning smarter and more effective
+        <p className="text-lightText mt-5">
+          Welcome to our platform dedicated to optimizing route planning for sales professionals.
+          Join us on this journey as we revolutionize the way salespeople navigate through their
+          daily tasks and gather crucial product insights. Together, let's make route planning
+          smarter and more effective.
         </p>
 
-         {authorized === false && ( // Use strict comparison (===) for clarity
-         <Link to="/login">
+        {!authorized && (
+          <Link to="/login">
             <button className="bg-white py-2 px-5 rounded-full mt-4 outline hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-blue-600 hover:text-white transition-all">
               Login
             </button>
           </Link>
         )}
-        </div>
+      </div>
 
-      <div className="w-full md:w-2/4 ml spa">
-        <img src="https://images.unsplash.com/photo-1548345680-f5475ea5df84?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hand holding phone with map displayed" />
+      <div className="w-full lg:w-2/4">
+        <img
+          className="w-full"
+          src="https://images.unsplash.com/photo-1548345680-f5475ea5df84?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Hand holding phone with map displayed"
+        />
       </div>
     </div>
   );
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { RiHome2Line, RiMapPinLine, RiSettings2Line } from "react-icons/ri";
 import { FaRegMessage } from "react-icons/fa6";
@@ -8,20 +8,49 @@ import { IoMenu } from "react-icons/io5";
 import { FaRegCalendarAlt } from "react-icons/fa";
 
 const SidebarItem = ({ icon, label, to }) => {
+  const [showLabel, setShowLabel] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowLabel(window.innerWidth > 968);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <Link to={to} className="flex items-center gap-4">
+    <Link to={to} className="flex items-center gap-4 py-2 pl-4">
       {icon}
-      <span className="text-lg font-semibold text-blue-600">{label}</span>
+      {showLabel && <span className="text-lg font-semibold text-blue-600">{label}</span>}
     </Link>
   );
 };
 
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showMenuText, setShowMenuText] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowMenuText(window.innerWidth > 968);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
+  const menuText = showMenuText ? 'Menu' : '';
 
   const sidebarItems = [
     { icon: <RiHome2Line className="h-6 w-6 text-blue-600" />, label: "Home", to: "/" },
@@ -34,17 +63,16 @@ const SideBar = () => {
   ];
 
   return (
-    <div className={`grid grid-rows-[auto,1fr] h-screen w-${isOpen ? '64' : '12'} mr-12 p-8 border-r border-gray-100 z-0`}>
-      <div className="flex items-center mb-8">
+    <div className="lg:w-64 lg:mr-12 lg:p-8 lg:border-r lg:border-gray-100 lg:z-0">
+      <div className="flex items-center mb-8 lg:hidden">
         <IoMenu className="h-8 w-8 cursor-pointer text-blue-600" onClick={toggleSidebar} />
-        {isOpen && <span className="ml-4 text-blue-600 text-lg font-bold">Menu</span>}
+        {isOpen && <span className="ml-4 text-blue-600 text-lg font-bold">{menuText}</span>}
       </div>
-      <div className={`flex flex-col gap-8 ${isOpen ? '' : 'hidden'}`}>
+      <div className={`flex flex-col gap-8 ${isOpen ? '' : 'hidden'} lg:flex lg:flex-col lg:gap-8 lg:flex-grow lg:overflow-auto`}>
         {sidebarItems.map((item, index) => (
           <SidebarItem key={index} {...item} />
         ))}
       </div>
-      <div></div>
     </div>
   );
 };
