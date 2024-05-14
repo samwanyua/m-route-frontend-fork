41958d7d06b3b304bcd950eb554a08ae2cb03a96
OumaArera <johnouma999@gmail.com>
2024-05-14 17:45:15 +0300

    fix: restrict access to API key

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { LoadScript } from "@react-google-maps/api"; // Import LoadScript instea
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <LoadScript
-      googleMapsApiKey={process.env.GOOGLE_MAP_API_KEY}
+      googleMapsApiKey={process.env.GOOLE_MAP_API_KEY}
     >
       <BrowserRouter>
         <App />
