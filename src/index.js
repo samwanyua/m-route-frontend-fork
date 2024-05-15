f2314cdf6a1beb440d2d0b8263265256e2d9726a
OumaArera <johnouma999@gmail.com>
2024-05-15 10:40:22 +0300

    fix: remove dotenv

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { LoadScript } from "@react-google-maps/api";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./.env"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
