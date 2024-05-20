93092e5d78e1fafa83972ecb1d4df306911a435c
OumaArera <johnouma999@gmail.com>
2024-05-20 03:28:54 +0300

    feat: reset forms after sending notifications

--- src/components/MerchRoutes.js
+++ src/components/MerchRoutes.js
@@ -8,7 +8,6 @@ const MerchRoutePlans = () => {
     const [routePlans, setRoutePlans] = useState([]);
     const [token, setToken] = useState(null);
     const [userId, setUserId] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState("");
     const [notification, setNotification] = useState(false);
     const [showForm, setShowForm] = useState(false);
@@ -36,7 +35,6 @@ const MerchRoutePlans = () => {
 
     
     const fetchData = async () => {
-        setIsLoading(true);
 
         try {
             const response = await fetch(`${ROUTES_URL}/${userId}`, {
@@ -111,6 +109,8 @@ const MerchRoutePlans = () => {
                     setNotification("");
                 }, 5000);
                 setShowForm(false);
+                setSelectedPlan("");
+                setNotificationsData({});
 
             }else{
                 setError(data.message);
@@ -130,6 +130,7 @@ const MerchRoutePlans = () => {
 
         if (name === "staff_no" && value < 0){
             alert("Please enter a positive value");
+            event.target.value = Math.max(0, value);
             return;
         }
         setNotificationsData(prev =>({
