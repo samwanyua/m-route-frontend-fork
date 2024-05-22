e1012e332b918f9b18b57cbf6d7ef5df7dae613b
OumaArera <johnouma999@gmail.com>
2024-05-22 12:45:03 +0300

    feat: reload page after successful notification

--- src/components/MerchRoutes.js
+++ src/components/MerchRoutes.js
@@ -16,6 +16,7 @@ const MerchRoutePlans = () => {
     const [selectedPlan, setSelectedPlan] = useState({});
     const [notificationsData, setNotificationsData] = useState({})
     const [managers, setManagers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const accessToken = localStorage.getItem("access_token");
@@ -133,10 +134,10 @@ const MerchRoutePlans = () => {
             const data = await response.json();
 
             if (data.status_code === 201){
-                setError(data.message);
+                setNotification(data.message);
                 setTimeout(() => {
-                    setError("");
-                }, 5000)
+                    setNotification("");
+                }, 5000);
 
             }else if(data.status_code === 400 || data.status_code === 404 || data.status_code === 500){
                 setError(data.message);
@@ -229,6 +230,7 @@ const MerchRoutePlans = () => {
         const notificationSent = await handleSubmit();
 
         if (instructionUpdated && notificationSent) {
+            setIsLoading(true);
             await fetchData();
         }
     };
@@ -238,102 +240,108 @@ const MerchRoutePlans = () => {
             <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-4 sm:mb-6 md:mb-8">Route Plans</h1>
             {error && <div className="text-red-500 mb-4">{error}</div>}
             {notification && <div className="text-green-500 mb-4">{notification}</div>}
-            <div className="overflow-x-auto">
-                <table className="min-w-full bg-white border border-gray-200">
-                    <thead>
-                        <tr>
-                            <th className="py-2 px-4 border-b">Facility</th>
-                            <th className="py-2 px-4 border-b">Instructions</th>
-                            <th className="py-2 px-4 border-b">Start Date</th>
-                            <th className="py-2 px-4 border-b">End Date</th>
-                            <th className="py-2 px-4 border-b">Status</th>
-                            <th className="py-2 px-4 border-b">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {routePlans.flatMap(plan => {
-                            const instructions = JSON.parse(plan.instructions);
-                            return instructions.map(instruction => (
-                                <tr key={`${plan.id}-${instruction.id}`} className="even:bg-gray-100">
-                                    <td className="py-2 px-4 border-b">{instruction.facility}</td>
-                                    <td className="py-2 px-4 border-b">{instruction.instructions}</td>
-                                    <td className="py-2 px-4 border-b">{moment(instruction.start, 'YYYY-MM-DDTHH:mm').format('DD/MM/YYYY h:mm A')}</td>
-                                    <td className="py-2 px-4 border-b">{moment(instruction.end, 'YYYY-MM-DDTHH:mm').format('DD/MM/YYYY h:mm A')}</td>
-                                    <td className="py-2 px-4 border-b">{instruction.status}</td>
-                                    <td className="py-2 px-4 border-b text-center">
-                                        <button
-                                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-                                            onClick={() => handleStatusChange(plan.id, instruction.id, instruction.status, instruction.facility)}
-                                        >
-                                            Send Notification
-                                        </button>
-                                    </td>
+            {isLoading ? (
+                <div className="text-center text-xl">Loading...</div>
+            ) : (
+                <>
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full bg-white border border-gray-200">
+                            <thead>
+                                <tr>
+                                    <th className="py-2 px-4 border-b">Facility</th>
+                                    <th className="py-2 px-4 border-b">Instructions</th>
+                                    <th className="py-2 px-4 border-b">Start Date</th>
+                                    <th className="py-2 px-4 border-b">End Date</th>
+                                    <th className="py-2 px-4 border-b">Status</th>
+                                    <th className="py-2 px-4 border-b">Action</th>
                                 </tr>
-                            ));
-                        })}
-                    </tbody>
-                </table>
-            </div>
-
-            {showForm && (
-                <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-                    <div className="bg-white p-4 rounded shadow-lg w-96">
-                        <h2 className="text-xl font-bold mb-4">Update Status</h2>
-                        <form onSubmit={handleFormSubmit}>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="managerSelect">Select Manager</label>
-                                <select
-                                    id="managerSelect"
-                                    name="staff_no"
-                                    value={notificationsData.staff_no}
-                                    onChange={handleSendNotification}
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    required
-                                >
-                                    <option value="">Select a manager</option>
-                                    {managerOptions}
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">Content</label>
-                                <textarea
-                                    id="content"
-                                    name="content"
-                                    value={notificationsData.content}
-                                    onChange={handleSendNotification}
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    required
-                                ></textarea>
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 text-sm font-bold mb-2">Status</label>
-                                <select
-                                    value={selectedPlan.status}
-                                    onChange={(e) => setSelectedPlan({ ...selectedPlan, status: e.target.value })}
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                >
-                                    <option value="pending">Pending</option>
-                                    <option value="complete">Complete</option>
-                                </select>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <button
-                                    type="submit"
-                                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                >
-                                    Submit
-                                </button>
-                                <button
-                                    type="button"
-                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                    onClick={() => setShowForm(false)}
-                                >
-                                    Cancel
-                                </button>
-                            </div>
-                        </form>
+                            </thead>
+                            <tbody>
+                                {routePlans.flatMap(plan => {
+                                    const instructions = JSON.parse(plan.instructions);
+                                    return instructions.map(instruction => (
+                                        <tr key={`${plan.id}-${instruction.id}`} className="even:bg-gray-100">
+                                            <td className="py-2 px-4 border-b">{instruction.facility}</td>
+                                            <td className="py-2 px-4 border-b">{instruction.instructions}</td>
+                                            <td className="py-2 px-4 border-b">{moment(instruction.start, 'YYYY-MM-DDTHH:mm').format('DD/MM/YYYY h:mm A')}</td>
+                                            <td className="py-2 px-4 border-b">{moment(instruction.end, 'YYYY-MM-DDTHH:mm').format('DD/MM/YYYY h:mm A')}</td>
+                                            <td className="py-2 px-4 border-b">{instruction.status}</td>
+                                            <td className="py-2 px-4 border-b text-center">
+                                                <button
+                                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                                                    onClick={() => handleStatusChange(plan.id, instruction.id, instruction.status, instruction.facility)}
+                                                >
+                                                    Send Notification
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ));
+                                })}
+                            </tbody>
+                        </table>
                     </div>
-                </div>
+
+                    {showForm && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+                            <div className="bg-white p-4 rounded shadow-lg w-96">
+                                <h2 className="text-xl font-bold mb-4">Update Status</h2>
+                                <form onSubmit={handleFormSubmit}>
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="managerSelect">Select Manager</label>
+                                        <select
+                                            id="managerSelect"
+                                            name="staff_no"
+                                            value={notificationsData.staff_no}
+                                            onChange={handleSendNotification}
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            required
+                                        >
+                                            <option value="">Select a manager</option>
+                                            {managerOptions}
+                                        </select>
+                                    </div>
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">Content</label>
+                                        <textarea
+                                            id="content"
+                                            name="content"
+                                            value={notificationsData.content}
+                                            onChange={handleSendNotification}
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            required
+                                        ></textarea>
+                                    </div>
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2">Status</label>
+                                        <select
+                                            value={selectedPlan.status}
+                                            onChange={(e) => setSelectedPlan({ ...selectedPlan, status: e.target.value })}
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                        >
+                                            <option value="pending">Pending</option>
+                                            <option value="complete">Complete</option>
+                                        </select>
+                                    </div>
+                                    <div className="flex items-center justify-between">
+                                        <button
+                                            type="submit"
+                                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                        >
+                                            Submit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                            onClick={() => setShowForm(false)}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
